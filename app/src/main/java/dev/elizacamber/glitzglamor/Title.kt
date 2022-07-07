a87d956f71efbb98ef2ab6c775085150e8321176
eliza <eliza@pixplicity.com>
2022-07-07 17:36:49 +0300

    [add] missing label

--- app/src/main/java/dev/elizacamber/glitzglamor/Title.kt
+++ app/src/main/java/dev/elizacamber/glitzglamor/Title.kt
@@ -1,7 +1,8 @@
 package dev.elizacamber.glitzglamor
 
 import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -9,10 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -24,14 +22,15 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun Title() {
-    val transitionData = updateTitleTransitionData()
+    val animState = remember { mutableStateOf(false) }
+    val transitionData = updateTitleTransitionData(animState)
 
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .height(128.dp)
             .background(transitionData.color)
-            .clickable { },
+            .clickable { animState.value = !animState.value },
         contentAlignment = Alignment.Center
     ) {
         Text(
@@ -51,27 +50,17 @@ private class TitleTransitionData(color: State<Color>, scale: State<Float>) {
 
 // Create a Transition and return its animation values.
 @Composable
-private fun updateTitleTransitionData(): TitleTransitionData {
-    val transition = rememberInfiniteTransition()
+private fun updateTitleTransitionData(isAnimated: MutableState<Boolean>): TitleTransitionData {
+    val transition = updateTransition(isAnimated, label = "title_animation")
 
     val bgColor1 = MaterialTheme.colorScheme.primary
     val bgColor2 = MaterialTheme.colorScheme.secondary
-    val bgColor = transition.animateColor(
-        initialValue = bgColor1,
-        targetValue = bgColor2,
-        animationSpec = infiniteRepeatable(
-            animation = tween(1000),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-    val scale = transition.animateFloat(
-        initialValue = 1f,
-        targetValue = 1.5f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(1000),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
+    val bgColor = transition.animateColor(label = "title_color") {
+        if (it.value) bgColor2 else bgColor1
+    }
+    val scale = transition.animateFloat(label = "title_scale") {
+        if (it.value) 1.5f else 1f
+    }
     return remember(transition) { TitleTransitionData(bgColor, scale) }
 }
 
