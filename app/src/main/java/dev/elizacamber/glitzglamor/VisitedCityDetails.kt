d663b6d75f05ed933fd97ca3c4f99702c154cac7
eliza <eliza@pixplicity.com>
2022-07-07 18:48:40 +0300

    [add] AnimatedVisibility slide + fade

--- app/src/main/java/dev/elizacamber/glitzglamor/VisitedCityDetails.kt
+++ app/src/main/java/dev/elizacamber/glitzglamor/VisitedCityDetails.kt
@@ -1,13 +1,18 @@
 package dev.elizacamber.glitzglamor
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,10 +32,15 @@ fun VisitedCityDetails(navController: NavHostController, country: String?, city:
 
 @Composable
 fun DetailsBanner(country: String, city: String) {
+
+    val visible = remember { mutableStateOf(false) }
+    val density = LocalDensity.current
+
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(128.dp),
+            .height(128.dp)
+            .clickable { visible.value = !visible.value },
         contentAlignment = Alignment.Center
     ) {
         if (getFlagForCountry(country) != null) {
@@ -42,12 +52,26 @@ fun DetailsBanner(country: String, city: String) {
                 alpha = 0.1f
             )
         }
-        Text(
-            text = city,
-            style = MaterialTheme.typography.titleLarge,
-            textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.onBackground
-        )
+        AnimatedVisibility(
+            visible = visible.value,
+            enter = slideInHorizontally {
+                // Slide in from 40 dp from the top.
+                with(density) { -100.dp.roundToPx() }
+            } + fadeIn(
+                // Fade in with the initial alpha of 0.3f.
+                initialAlpha = 0.3f
+            ),
+            exit = slideOutHorizontally {
+                with(density) { +200.dp.roundToPx() }
+            } + fadeOut()
+        ) {
+            Text(
+                text = city,
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
     }
 }
 
